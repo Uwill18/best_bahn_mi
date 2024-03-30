7c2ed1665897608b8908a092111ccf22080d2a8a
Hoshi Holiday <hoshiholiday@gmail.com>
2024-03-30 15:17:01 -0500

    new logo
    adding new logo and decoration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+/*Matthew 4:4; 1 Corinthians 10:31*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
