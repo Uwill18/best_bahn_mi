e7a8db10d018c0efb99e320b5533be15c491d0b7
Hoshi Holiday <hoshiholiday@gmail.com>
2024-03-29 19:33:53 -0500

    Bahn mi begins
    banner and button is set up

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './Header.module.css'
 import bannerImage from '../../assets/bahn_mi_banner.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = () => {
   return (
     <>
     <header className={classes.header}>
         <h1>Best Bahn Mi</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
     </header>
     <div className={classes["main-image"]}>
         <img src={bannerImage} alt="Banh Mi Sandwich Banner"/>
