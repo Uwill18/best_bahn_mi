79145dae2f220d40988e1398e7b26ecd18d02139
Hoshi Holiday <hoshiholiday@gmail.com>
2024-04-01 15:21:57 -0500

    Cart functionality
    updating cart functions

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import classes from './MealItem.module.css'
 import MealItemForm from '../MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
 
-const price = `${props.price.toFixed(2)}`;
+const cartCtx = useContext(CartContext);
+
+const price = `$${props.price.toFixed(2)}`;
+
+const addToCartHandler = (amount) => {
+  cartCtx.addItem({
+    id: props.id,
+    name: props.name,
+    amount: amount,
+    price: props.price,
+  });
+};
 
 
   return (
@@ -12,9 +24,9 @@ const price = `${props.price.toFixed(2)}`;
         <div>
             <h3>{props.name}</h3>
             <div className={classes.description}>{props.description}</div>
-            <div className={classes.price}>${price}</div>
+            <div className={classes.price}>{price}</div>
         </div>
-        <div><MealItemForm id={props.id}/></div>
+        <div><MealItemForm id={props.id} onAddToCart={addToCartHandler}/></div>
     </li>
   
   )
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import classes from './Modal.module.css'
+
+const portalElement = document.getElementById("overlays");
+const ModalOverlay = (props) =>{
+    return(
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    )
+}
+const Modal = (props) => {
+  return (
+    <>
+    {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </>
+  )
+}
+
+export default Modal
